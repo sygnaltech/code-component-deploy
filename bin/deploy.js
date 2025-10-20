cac38b26677931ed3d336f0ec7a594bdd6a5e7ce
Michael Wells <mike@sygnal.com>
2025-10-20 16:20:16 +1300

    Telemetry + API key support

--- bin/deploy.js
+++ bin/deploy.js
@@ -16,6 +16,12 @@ for (let i = 0; i < args.length; i++) {
     } else if (arg === '--main-branch' && args[i + 1]) {
         options.mainBranch = args[i + 1];
         i++;
+    } else if (arg === '--api-token' && args[i + 1]) {
+        options.apiToken = args[i + 1];
+        i++;
+    } else if (arg === '--manifest' && args[i + 1]) {
+        options.manifest = args[i + 1];
+        i++;
     } else if (arg === '--help' || arg === '-h') {
         console.log(`
 sygnal-deploy - Deploy Webflow component libraries
@@ -25,12 +31,15 @@ Usage: sygnal-deploy [options]
 Options:
   --version-file <path>   Path to version file (default: src/version.ts)
   --main-branch <name>    Name of main branch (default: main)
+  --api-token <token>     Webflow API token for deployment
+  --manifest <path>       Path to webflow.json manifest (default: webflow.json)
   --help, -h              Show this help message
 
 Examples:
   sygnal-deploy
   sygnal-deploy --version-file src/constants/version.ts
   sygnal-deploy --main-branch master
+  sygnal-deploy --api-token abc123 --manifest webflow.json
         `);
         process.exit(0);
     }
